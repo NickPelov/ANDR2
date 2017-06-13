114aafc354b12921cc7a47791fdb2dfc99524dc0
yasengeorgiev <yasen_96@abv.bg>
2017-06-13 23:41:05 +0200

    Loading image from gallery by pressing on the ImageView

--- app/src/main/java/com/example/owner/android2/ProfileActivity2.java
+++ app/src/main/java/com/example/owner/android2/ProfileActivity2.java
@@ -2,7 +2,11 @@ package com.example.owner.android2;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -15,7 +19,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.facebook.login.widget.ProfilePictureView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileActivity2 extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,6 +32,8 @@ public class ProfileActivity2 extends AppCompatActivity
     TextView nickNameTextView;
     TextView emailTextView;
     TextView pointsTextView;
+    ImageView imageView;
+    public static final int RESULT_LOAD_IMAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +68,41 @@ public class ProfileActivity2 extends AppCompatActivity
         nickNameTextView.setText(CurrentUser.getUser().NickName);
         emailTextView.setText(CurrentUser.getUser().Email);
         pointsTextView.setText(String.valueOf(CurrentUser.getUser().Score));
+
+        imageView = (ImageView) findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.image);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.profile_activity2, menu);
