556e489649718560f366780fc02bee0b599d18a1
DESKTOP-T7UD5E9\k_vol <k_volodiev@abv.bg>
2017-06-18 16:10:35 +0200

    1st unit test

--- app/src/test/java/com/example/owner/android2/ExampleUnitTest.java
+++ app/src/test/java/com/example/owner/android2/ExampleUnitTest.java
@@ -1,5 +1,10 @@
 package com.example.owner.android2;
 
+import com.example.owner.android2.User.CurrentUser;
+import com.example.owner.android2.User.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,4 +19,38 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void Login() throws Exception {
+        String UserEmail = "admin@";
+        String UserPass = "ad";
+        for (User user : CurrentUser.users) {
+            if (user.Email.equals(UserEmail)) {
+                // Account exists, return true if the password matches.
+                if (user.Password.equals(UserPass)) {
+                    assertEquals(UserPass, user.Password);
+                }
+                assertEquals(UserEmail, user.Email);
+            }
+        }
+    }
+//    mnogo exeptioni
+//    @Test
+//    public void Register() throws Exception {
+//        String UserEmail = "testRegister@";
+//        String UserPass = "pass";
+//        String UserNick = "testRegister";
+//        String UserName = "testRegister";
+//        DatabaseReference usersTable = FireBaseConnection.mRootRefForTest.child("users").push();
+//        String pushId = usersTable.getKey();
+//        usersTable.setValue(new User(UserName, false, UserEmail, UserNick, UserPass, 0, new location(0, 0)));
+//        for (User user : CurrentUser.users) {
+//            if (user.Email.equals(UserEmail)) {
+//                // Account exists, return true if the password matches.
+//                if (user.Password.equals(UserPass)) {
+//                    assertEquals(UserPass, user.Password);
+//                }
+//                assertEquals(UserEmail, user.Email);
+//            }
+//        }
+//    }
 }
